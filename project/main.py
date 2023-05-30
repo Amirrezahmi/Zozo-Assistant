f4576cad7c2e463c300be9c955a5f527381e2100
Amirreza Hashemi <89692207+Amirrezahmi@users.noreply.github.com>
2023-05-30 16:38:03 -0400

    Update main.py

--- project/main.py
+++ project/main.py
@@ -48,7 +48,7 @@ def api_not_worked():
 #weather
 def weather(location, country):
   # Returns full JSON object
-  apiKey = 'b30305181423db1df9a1e8212c3f0504' # Enter your API Key here for weather
+  apiKey = '' # Enter your API Key here for weather
   base = 'http://api.openweathermap.org/data/2.5/weather?q='
   url = base + location + ',' + country + '&units=metric&appid=' + apiKey
   response = requests.get(url).json()
